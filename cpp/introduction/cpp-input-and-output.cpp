0bdd9565538592ce3509ad28e93d376d6169391b
Muhamad Bayu Wilanda <muhamadbayuwilanda@protonmail.com>
2016-08-09 11:40:24 +0700

    Create cpp-input-and-output.cpp
    2nd challenge

---
+++ cpp/introduction/cpp-input-and-output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int st,nd,th;
+    
+    cin >> st >> nd >> th;
+    cout << st + nd + th <<endl;/* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
